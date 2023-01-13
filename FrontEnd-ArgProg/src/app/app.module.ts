1a1959faab97c9051d7c21f9be7391825a9f25d6
sermass <31020145+sermass@users.noreply.github.com>
2023-01-12 22:04:22 -0300

    agrego acerca de y perfil

--- FrontEnd-ArgProg/src/app/app.module.ts
+++ FrontEnd-ArgProg/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercaDeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
