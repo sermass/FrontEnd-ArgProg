5849ba78ce92aa166af203087e8b2dea693b70fd
sermass <31020145+sermass@users.noreply.github.com>
2023-01-14 07:49:25 -0300

    agrego los componentes "experiencia y educacion" y "hard y soft skill"

--- FrontEnd-ArgProg/src/app/app.module.ts
+++ FrontEnd-ArgProg/src/app/app.module.ts
@@ -6,17 +6,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardSoftSkilComponent } from './components/hard-soft-skil/hard-soft-skil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AcercaDeComponent,
-    PerfilComponent
+    PerfilComponent,
+    ExperienciaComponent,
+    HardSoftSkilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontEnd-ArgProg/src/app/components/hard-soft-skil/hard-soft-skil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-soft-skil',
+  templateUrl: './hard-soft-skil.component.html',
+  styleUrls: ['./hard-soft-skil.component.css']
+})
+export class HardSoftSkilComponent {
+
+}
