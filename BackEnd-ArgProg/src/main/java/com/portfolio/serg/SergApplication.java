2f496b2fcebf12c68ede3a854e192834aeb8e2b6
sermass <31020145+sermass@users.noreply.github.com>
2023-01-31 07:43:16 -0300

    cambio de nombre de proyecto y agregado de backend con estructura spring

---
+++ BackEnd-ArgProg/src/main/java/com/portfolio/serg/SergApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.serg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergApplication.class, args);
+	}
+
+}
---
+++ BackEnd-ArgProg/src/test/java/com/portfolio/serg/SergApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.serg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
